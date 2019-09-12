9ea600f37393edd9d5f52a429ccf28030deaab52
CN72 <cn72@CN72.local>
2019-09-12 17:59:21 +0100

    Updates

---
+++ precache-manifest.265dac8f1866babf27d63f997969b42a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279e8751abd9981e7ad09213502b926a",
+    "url": "/simple-address-book/./index.html"
+  },
+  {
+    "revision": "581261726769d7fd5adf",
+    "url": "/simple-address-book/./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "54c318f7f506a75a1a0e",
+    "url": "/simple-address-book/./static/js/2.839a3c53.chunk.js"
+  },
+  {
+    "revision": "581261726769d7fd5adf",
+    "url": "/simple-address-book/./static/js/main.cc6bd0d9.chunk.js"
+  },
+  {
+    "revision": "3b9cb0716731feaa9318",
+    "url": "/simple-address-book/./static/js/runtime~main.849fc073.js"
+  }
+]);
